192417c637c9d79d5f7532305e5b5c059c5cd0ac
Ricardo Maldonado <rcmalonso@gmail.com>
2020-10-15 00:17:38 -0700

    finish purchase history component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,13 +8,91 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: 8.02
+                amount: 19.40,
+                orderNumber: 'ADSF551D34D',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 19.40
+                amount: 9.42,
+                orderNumber: 'ADSF551D34D',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Maxwell Nelson',
+                    shippingAddress: '745 West State Street'
+                }
+            },
+            {
+                _id: 2,
+                amount: 1.30,
+                orderNumber: 'ADSF551D34D',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Ricardo Maldonado',
+                    shippingAddress: '400 Scarborough Cir'
+                }
+            },
+            {
+                _id: 3,
+                amount: 4.15,
+                orderNumber: 'P876VIU7IU55',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Cesar Alonso',
+                    shippingAddress: '645 Shoreview Dr'
+                }
+            },
+            {
+                _id: 4,
+                amount: 5.90,
+                orderNumber: 'ADUYT6870UHG',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Hertty Bolanos',
+                    shippingAddress: '745 West State Circle'
+                }
+            },
+            {
+                _id: 5,
+                amount: 3.45,
+                orderNumber: 'KJGFHDT67943',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Kyle Simmons',
+                    shippingAddress: '2140 East State Street'
+                }
+            },
+            {
+                _id: 6,
+                amount: 5.45,
+                orderNumber: 'KUFKTHD745785',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Gene Roberts',
+                    shippingAddress: '2120 South State Street'
+                }
+            },
+            {
+                _id: 7,
+                amount: 8.75,
+                orderNumber: 'I5IIY7EDOP964',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Glen Benton',
+                    shippingAddress: '680 North State Street'
+                }
             }
         ]
     })
