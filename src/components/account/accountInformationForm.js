4818f8236cb563b2e47d1f4592cf2b00567b7d25
Ricardo Maldonado <rcmalonso@gmail.com>
2020-10-26 22:25:02 -0700

    added change password button

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
-import { FormInput, FormButton } from '../formFields';
+import { FormInput, FormButton, LongGrayButton } from '../formFields';
 
 import history from '../../history';
 
@@ -11,11 +11,19 @@ class AccountInformationForm extends Component {
         const { className, handleSubmit } = this.props;
         
         return (
-            <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
-                <Field className='sign-in-form__email' name='email' type='email' title='Email' placeholder='Email' component={FormInput} />
-                <Field className='sign-in-form__password' name='password' type='password' title='Password' placeholder='Password' component={FormInput} />
-                <div className='sign-in-form__line'></div>
-                <Field className='sign-in-form__login' onClick={() => history.push('account')} name='login' type='submit' title='Login'  component={FormButton} />
+            <form onSubmit={handleSubmit} className={`${className} account-information-form`}>
+                <Field className='account-information-form__name' name='name' type='name' title='Name' placeholder='Name' component={FormInput} />
+                <Field className='account-information-form__email' name='email' type='email' title='Email' placeholder='Email' component={FormInput} />
+
+                <Field className='account-information-form__street-address' name='address' type='address' title='Street Address' placeholder='Street Address' component={FormInput} />
+                <Field className='account-information-form__city' name='city' type='city' title='City' placeholder='City' component={FormInput} />
+
+                <Field className='account-information-form__state' name='state' type='state' title='State' placeholder='State' component={FormInput} />
+                <Field className='account-information-form__zipcode' name='zipcode' type='zipcode' title='Zipcode' placeholder='Zipcode' component={FormInput} />
+
+                <Field className='account-information-form__password' name='password' type='password' title='Password' placeholder='Password' component={FormInput} />
+                
+                <Field className='account-information-form__change-password' onClick={() => console.log('trying to show password')} name='change-password' type='button' labelTitle='Password' title='Change Password'  component={LongGrayButton} />
             </form>
         )
     }
