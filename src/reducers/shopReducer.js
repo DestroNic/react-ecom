3ea91322205b117ffc8c0bd8b25ae9c9c2a36e6a
Ricardo Maldonado <rcmalonso@gmail.com>
2020-10-30 18:32:22 -0700

    navbar refactor

--- src/reducers/shopReducer.js
+++ src/reducers/shopReducer.js
@@ -1,21 +1,36 @@
 import {
-    SET_SHOP_PRODUCTS
+    SET_SHOP_PRODUCTS,
+    FILTER_PRODUCTS_WITH_CATEGORY_ID,
+    SET_SHOP_CATEGORIES
 
 } from '../actions/types';
 
 const INITIAL_STATE = {
     categories: [], 
-    selectedCategoryId: 0,
-    productSelected: []
+    productSelected: [],
+    products: []
 }
 
 export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
-        case SET_SHOP_PRODUCTS:
+        case SET_SHOP_CATEGORIES:
+            
             return {
                 ...state,
                 categories: action.payload
             }
+        case SET_SHOP_PRODUCTS:
+            return {
+                ...state,
+                products: action.payload
+            }
+            case FILTER_PRODUCTS_WITH_CATEGORY_ID:
+                console.log(action.payload);
+                return {
+                    ...state,
+                    // selectedCategoryId
+                }
+
         default: return state;
     }
 }
