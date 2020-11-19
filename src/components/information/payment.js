1ccc63a9dc77bc5dba3fcff26b7dd0a23a486f83
Ricardo Maldonado <rcmalonso@gmail.com>
2020-11-18 23:44:54 -0800

    structured shipping js and and shipping form

--- src/components/information/payment.js
+++ src/components/information/payment.js
@@ -4,7 +4,9 @@ import PageTitle from '../pageTitle';
 
 
 import { connect } from 'react-redux';
-import * as actions from '../../actions'
+import * as actions from '../../actions';
+
+import PaymentForm from './paymentForm';
 
 
 class Payment extends Component {
@@ -22,7 +24,7 @@ class Payment extends Component {
         return (
             <div className='sign-in'>
                 <PageTitle className='sign-in__page-title' title='Payment Information' />
-                {/* <PaymentForm onSubmit={this.onSubmit} className='sign-in__form' /> */}
+                <PaymentForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
         )
     }
